0c089037fc9f2c3466d13c9c1bcfe79bfdb75fc4
unknown <enzohorquin@gmail.com>
2019-05-03 17:39:53 -0300

    Apps running

--- app.js
+++ app.js
@@ -11,7 +11,8 @@ var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.set('view engine', 'ejs');
+
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -35,7 +36,10 @@ app.use(function(err, req, res, next) {
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.render('error', { error: err });
 });
+app.listen(3000, function () {
+  console.log("Express server listening on port 3000");
+  });
 
 module.exports = app;
